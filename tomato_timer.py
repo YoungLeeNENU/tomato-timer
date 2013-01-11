1501b6d39de6670165124c32ea67f944e5afbeb6
YoungLee <YoungLeeNENU@gmail.com>
2013-01-11 13:21:41 +0800

    can work

--- tomato_timer.py
+++ tomato_timer.py
@@ -1,11 +1,34 @@
 # -*- coding:utf-8 -*-
 ## 番茄工作法的计时器
-## 工作15分钟
-## 休息3分钟
-import os
+## 默认工作15分钟
+## 默认休息3分钟
 import sys
+import gtk
 import time
+import pygtk
 import datetime
+
+def createWindow_work():
+        window = gtk.Window()
+        window.set_default_size(300, 300)
+        window.connect('destroy', gtk.main_quit)
+
+        label = gtk.Label('Go to Rest!')
+        window.add(label)
+
+        label.show()
+        window.show()
+
+def createWindow_rest():
+        window = gtk.Window()
+        window.set_default_size(300, 300)
+        window.connect('destroy', gtk.main_quit)
+
+        label = gtk.Label('Back to work!')
+        window.add(label)
+
+        label.show()
+        window.show()
 
 table = {'work_trigger': 15, 'rest_trigger': 3}
 
@@ -29,8 +52,8 @@ def q1():
 def q2():
 	print "Rest time(min):"
 	while 1:
+		rest = raw_input()
 		try:
-			rest = raw_input()
 			if int(rest) > 0:
 				table['rest_trigger'] = rest
 				break
@@ -46,5 +69,25 @@ def q2():
 q1()
 q2()
 
-timeA = datetime.datetime.now()
-print timeA
+work_sec = int(table['work_trigger']) * 60
+rest_sec = int(table['rest_trigger']) * 60
+
+while 1:
+	while 1:
+		work_sec = work_sec - 1
+		time.sleep(1)
+		if work_sec <= 0:
+			createWindow_work()
+			gtk.main()
+			break
+
+	while 1:
+		rest_sec = rest_sec - 1
+		time.sleep(1)
+		if rest_sec <= 0:
+			createWindow_rest()
+			gtk.main()
+			break
+
+	work_sec = int(table['work_trigger']) * 60
+	rest_sec = int(table['rest_trigger']) * 60
